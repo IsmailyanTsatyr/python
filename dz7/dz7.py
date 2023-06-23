e89bc00153d09506cca72d09ed6b1185766b7167
Tsatyr Ismailyan <palint1@mail.ru>
2023-06-23 16:30:37 +0300

    dz7

---
+++ dz7/dz7.py
@@ -0,0 +1,37 @@
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны. 
+# Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). Примечание: бинарной операцией 
+# называется любая операция,у которой ровно два аргумента, как, например, у операции умножения.
+
+#РЕШЕНИЕ
+
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+#     for i in a:
+#         print(*[f"{x:>3}" for x in i])
+
+
+# print_operation_table(lambda x, y: x * y)
+
+#-----------------------------------------------------------------------------------------------------
+
+#Задача 34:
+
+#РЕШЕНИЕ   
+
+
+# line = input().lower()
+# line = "если-я-чушу в-затылке-не-беда"
+# lines = line.split()
+ 
+# print(lines)
+ 
+# lst = [sum(x in 'уеыаоэяию' for x in lin)
+#  for lin in lines]
+ 
+# if len(set(lst)) == 1 :
+#     res = "Парам пам-пам"  
+# else: res = "Пам парам"
+ 
+# print(res)
